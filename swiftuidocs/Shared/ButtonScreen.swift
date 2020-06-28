cad68457ff4f98080a906a5feaaf2079ee65ef3f
Anton Kuznetsov <isnifer@gmail.com>
2020-06-28 15:44:43 +0300

    Added Button Screen

---
+++ swiftuidocs/Shared/ButtonScreen.swift
@@ -0,0 +1,22 @@
+//
+//  ButtonScreen.swift
+//  Shared
+//
+//  Created by Anton Kuznetsov on 28.06.2020.
+//
+
+import SwiftUI
+
+struct ButtonScreen: View {
+  var body: some View {
+    Button(action: {}, label: {
+      Text("Button")
+    })
+  }
+}
+
+struct ButtonScreen_Previews: PreviewProvider {
+  static var previews: some View {
+    ButtonScreen()
+  }
+}
--- swiftuidocs/Shared/swiftuidocsApp.swift
+++ swiftuidocs/Shared/swiftuidocsApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct swiftuidocsApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ButtonScreen()
     }
+  }
 }
